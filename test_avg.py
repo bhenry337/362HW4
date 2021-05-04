c3b152bd90f08f847c852f22a4a988052910b18b
bhenry337 <72040662+bhenry337@users.noreply.github.com>
2021-05-03 21:10:49 -0700

    final touches

--- test_avg.py
+++ test_avg.py
@@ -5,8 +5,8 @@ class TestCase(unittest.TestCase):
 	def test_pos(self):
 		self.assertEqual(list_avg.avg([2,3,4]),3)
 
-	#def test_neg(self):
-	#	self.assertEqual(list_avg.avg([]),0)
+	def test_neg(self):
+		self.assertEqual(list_avg.avg([]),0)
 
-	#def test_text(self):
-	#	self.assertEqual(list_avg.avg(["erer", 2, "stop"]),"Incorrect Input")		
+	def test_text(self):
+		self.assertEqual(list_avg.avg(["erer", 2, "stop"]),"Incorrect Input")		
